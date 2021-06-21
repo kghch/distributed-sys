b70ca50524271e6d96a01c5b85f4f0d2933d3a67
hwang23 <hwang23@ebay.com>
2021-06-21 21:26:26 +0800

    lab1 mapreduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,16 +1,43 @@
 package mr
 
-import "log"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sync"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+var mu sync.Mutex
 
 type Coordinator struct {
 	// Your definitions here.
-
+	//mu sync.Mutex
+	idCounter int
+	workerIdCount int
+	fileNames []string
+	mapTasks []Task
+	reduceTasks []Task
 }
+
+type Task struct {
+	Id int
+	Ttype int // 0:map, 1:reduce, 2:wait
+	FileName string
+	Content string // param for mapf
+
+	TmpFiles []string // output of mapf & input of reducef
+	OutputFile string // output of reducef
+
+	Status int // 0:not assigned, 1:success, 2:fail, 3:running
+	NReduce int
+	TaskIds []int
+	//WorkerId int
+}
+
 
 // Your code here -- RPC handlers for the worker to call.
 
@@ -24,6 +51,71 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
+func (c *Coordinator) AssignTask(req *TaskReq, reply *TaskResp) error {
+	mu.Lock()
+	defer mu.Unlock()
+	mapFinished := true
+	for i, _ := range c.mapTasks {
+		if c.mapTasks[i].Status == 0 {
+			c.mapTasks[i].Status = 3
+			//c.mapTasks[i].WorkerId = req.WorkerId
+			reply.Task = &c.mapTasks[i]
+			return nil
+		}
+		if c.mapTasks[i].Status != 1 {
+			mapFinished = false
+		}
+	}
+
+	if !mapFinished {
+		reply.Task = &Task{}
+		reply.Task.Ttype = 2
+		return nil
+	}
+
+	for i, _ := range c.reduceTasks {
+		if c.reduceTasks[i].Status == 0 {
+			c.reduceTasks[i].Status = 3
+			//c.reduceTasks[i].WorkerId = req.WorkerId
+			reply.Task = &c.reduceTasks[i]
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func (c *Coordinator) ReportTask(req *ReportReq, reply *ReportResp) error {
+	mu.Lock()
+	defer mu.Unlock()
+	for i, task := range c.mapTasks {
+		if task.Id == req.Task.Id {
+			c.mapTasks[i].Status = req.Task.Status
+			c.mapTasks[i].TmpFiles = req.Task.TmpFiles
+		}
+	}
+
+
+	for i, task := range c.reduceTasks {
+		if task.Id == req.Task.Id {
+			c.reduceTasks[i].Status = req.Task.Status
+			c.reduceTasks[i].OutputFile = req.Task.OutputFile
+			break
+		}
+	}
+
+
+	reply.Success = true
+	return nil
+}
+
+func (c *Coordinator) GetId(req *GetIdReq, reply *GetIdResp) error {
+	mu.Lock()
+	defer mu.Unlock()
+	c.workerIdCount ++
+	reply.Id = c.workerIdCount
+	return nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -50,7 +142,6 @@ func (c *Coordinator) Done() bool {
 
 	// Your code here.
 
-
 	return ret
 }
 
@@ -60,11 +151,55 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	mu = sync.Mutex{}
+	c := Coordinator{
+		fileNames: files,
+		mapTasks: []Task{},
+		reduceTasks: []Task{},
+	}
 
 	// Your code here.
+	taskIds := make([]int, 0)
+	for i:=0; i<len(files);i++ {
+		taskIds = append(taskIds, i)
+	}
 
+	for i:=0;i<nReduce;i++ {
+		c.reduceTasks = append(c.reduceTasks, Task{
+			Id: i+100,
+			Ttype: 1,
+			TmpFiles: []string{},
+			Status: 0,
+			NReduce: nReduce,
+			TaskIds: taskIds,
+		})
+	}
+	fmt.Println("c.reduceTasks", len(c.reduceTasks), c.reduceTasks)
 
+	for i:=0;i<len(files);i++ {
+		oneTask := Task{
+			Id: i,
+			Ttype: 0,
+			FileName: files[i],
+			Content: "",
+			TmpFiles: []string{},
+			Status: 0,
+			NReduce: nReduce,
+			TaskIds: taskIds,
+		}
+		file, err := os.Open(files[i])
+		if err != nil {
+			log.Fatalf("cannot open %v", files[i])
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", files[i])
+		}
+		file.Close()
+		oneTask.Content = string(content)
+		c.mapTasks = append(c.mapTasks, oneTask)
+	}
+	fmt.Println("task assign finished.")
 	c.server()
 	return &c
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,9 +1,17 @@
 package mr
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
+
 
 
 //
@@ -13,6 +21,12 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -25,17 +39,113 @@ func ihash(key string) int {
 }
 
 
+func handleMapTask(task *Task, mapf func(string, string) []KeyValue) {
+	mapRes := mapf(task.FileName, task.Content)
+	files := make([]string, 0)
+	for _, kv := range mapRes {
+		filename := fmt.Sprintf("mr-tmp-%v-%v", task.Id, ihash(kv.Key) % task.NReduce)
+		f, _ := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		f.WriteString(kv.Key+" "+kv.Value + "\n")
+		f.Close()
+		files = append(files, filename)
+	}
+
+	task.TmpFiles = files
+	task.Status = 1
+	reportTask(task)
+}
+
+func handleReduceTask(task *Task, reducef func(string, []string) string) {
+	outFile := fmt.Sprintf("mr-out-%v", task.Id-100)
+	f, _ := os.OpenFile(outFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	intermediate := []KeyValue{}
+	for _, j := range task.TaskIds {
+		filename := fmt.Sprintf("mr-tmp-%v-%v", j, task.Id-100)
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			arr := strings.SplitN(line, " ", 2)
+			intermediate = append(intermediate, KeyValue{arr[0], arr[1]})
+		}
+	}
+
+
+	sort.Sort(ByKey(intermediate))
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		f.WriteString(intermediate[i].Key + " " + output + "\n")
+		i = j
+	}
+
+	task.OutputFile = outFile
+	task.Status = 1
+	reportTask(task)
+}
+
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
+
 	// Your worker implementation here.
+	workerId := os.Getuid()
+	for {
+		task := askForTask(workerId)
+		if task == nil {
+			return
+		}
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+		if task.Ttype == 0 {
+			handleMapTask(task, mapf)
+		} else if task.Ttype == 1 {
+			handleReduceTask(task, reducef)
+		} else if task.Ttype == 2 {
+			time.Sleep(3*time.Second)
+		}
+	}
+}
 
+func askForTask(workerId int) *Task {
+	args := TaskReq{
+		WorkerId: workerId,
+	}
+	var reply TaskResp
+	call("Coordinator.AssignTask", &args, &reply)
+	return reply.Task
+}
+
+func reportTask(task *Task) bool {
+	args := ReportReq{
+		Task: task,
+	}
+	var reply ReportResp
+	call("Coordinator.ReportTask", &args, &reply)
+	return reply.Success
+}
+
+func getId() int {
+	args := GetIdReq{}
+	var reply GetIdResp
+	call("Coordinator.GetId", &args, &reply)
+	return reply.Id
 }
 
 //
